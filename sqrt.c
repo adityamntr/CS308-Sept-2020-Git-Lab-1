d8eac4610df07fc65a69fa18a956b88f2614f2e9
Aditya Mantri <adityamntr@gmail.com>
2020-10-14 16:38:01 +0530

    handling non number inputs.

--- sqrt.c
+++ sqrt.c
@@ -4,15 +4,27 @@ positive number. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+
 //this is a comment
 int main(int argc, char* argv[]) {
-		
+
+	char zero[] = "0";
+
 	if (argc != 2) {
 		printf("Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
+
+	if(input == 0)
+	if(strcmp(zero, argv[1]) != 0)
+	{
+		printf("The input is not a valid number. \n");
+		exit(-1);
+	}
+
 	if(input >= 0)
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	else
